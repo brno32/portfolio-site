e23351a1de445722ab114e8df917bcdd3c6c30ea
Alex Drozd <drozdster@gmail.com>
2018-08-11 23:33:39 -0500

    adds single blog view

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField(default=timezone.now)
     body = models.CharField(max_length=1600)
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_cleaned(self):
+        return self.pub_date.strftime('%b %e, %Y')
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def allblogs(request):
@@ -6,3 +6,7 @@ def allblogs(request):
     blogs = Blog.objects.all()
 
     return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    detail_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'detail_blog': detail_blog})
