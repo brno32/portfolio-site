33099c97ef45bcb53aefc6b5ed979bc7769b1e45
Alex Drozd <drozdster@gmail.com>
2018-08-12 18:58:03 -0500

    base.html now extends all html

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -44,8 +44,12 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                 "templates")],
+        'DIRS': [
+            os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "templates"
+            ),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
