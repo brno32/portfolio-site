e7ef61f799f97b5c4ae251215cf9b75cd9400695
Alex Drozd <drozdster@gmail.com>
2018-08-13 19:06:46 -0500

    adds functionality to portfolio item links

--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
-class Job(models.Model): 
+class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    note = models.CharField(max_length=80, default='')
+    github_link = models.CharField(max_length=100, default='')
+    use_link = models.CharField(max_length=100, default='')
