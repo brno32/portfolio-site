3d25d79e13dcfc4ed50f35a03fc7415a797f1f7a
Alex Drozd <drozdster@gmail.com>
2018-08-13 20:11:50 -0500

    cleans up main url file and removes accidently committed .pyc files

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import blog.views
 import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('', jobs.views.home, name='home')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', jobs.views.home, name='home'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
